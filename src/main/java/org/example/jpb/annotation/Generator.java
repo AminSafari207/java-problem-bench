0711e62ee1d68757f6c8569d06944dea9aa7e244
AminSafari207 <safarin207368@gmail.com>
2026-06-28 16:15:39 +0330

    feat(annotation): add core problem benchmark annotations

---
+++ src/main/java/org/example/jpb/annotation/Generator.java
@@ -0,0 +1,14 @@
+package org.example.jpb.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Generator {
+    int iterations() default 100;
+    long seed() default 42L;
+}

90772523baaba6292536aa45b992cf7b490d3d78
AminSafari207 <safarin207368@gmail.com>
2026-07-10 10:27:28 +0330

    refactor(annotation): rename @Case to @CaseSet and add id and displayName to it

--- src/main/java/org/example/jpb/annotation/CaseSet.java
+++ src/main/java/org/example/jpb/annotation/CaseSet.java
@@ -8,4 +8,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.FIELD })
 public @interface CaseSet {
+	String id();
+
+	String displayName() default "";
 }
